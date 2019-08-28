96befadf8e3ee158510dbcff2a31976379a5625c
Salmi Johannes <s1901283@edu.bc.fi>
2019-08-28 13:48:48 +0300

    ensimmäinen commit
    ensimmäinen commit

---
+++ src/blackjack/Korttipakka.java
@@ -0,0 +1,21 @@
+package blackjack;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ */
+public class Korttipakka {
+//    Toteuta luokka Korttipakka, jolla on ArrayList<Kortti>-tyyppinen 
+//    atribuutti pakka. Luokan oletuskonstruktori sijoittaa pakka-atribuuttiin 
+//    halutun määrän 52 korttisia korttipakkoja numero- ja maajärjestyksessä. 
+//    Toteuta luokkaan metodi sekoita, joka sekoittaa atribuutin pakka korttien 
+//    järjestyksen. Toteuta lisäksi metodi jaaKortti, joka palauttaa pakan 
+//    ensimmäisen kortin ja poistaa sen pakasta.
+    
+}
---
+++ src/blackjack/PelaajanKasi.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blackjack;
+
+/*
+ *
+ * @author 
+ */
+public class PelaajanKasi {
+
+//    Toteuta luokka PelaajanKasi, jolla on ArrayList<Kortti>-tyyppinen atribuutti 
+//    kortit. Toteuta luokkaan metodi otaKortti, joka lisää kortit-listaan 
+//    parametrina saamansa kortin. Toteuta luokkaan lisäksi metodi selvitäSumma, 
+//    joka selvittää pelaajan käden summan seuraavin säännöin:
+//    
+//    kortin arvo 2-10:         käden summaan lisätään kortin numeroa vastaava arvo
+//    kortin arvo 11-13:        käden summaan lisätään 10
+//    kortin arvo 1 (ässä):	käden summaan lisätään 11
+//
+//    Mikäli korttein yhteissummaksi tuli yli 21, niin vähennetään summasta 10. 
+//    Tässä tilanteessa on saatu kaksi ässää, jolloin toisen ässän tulkitaan 
+//    olevan arvoltaan 1.    
+//    
+//    
+//    Blackjackiksi kutsutaan tilannetta , jolla pelaajalla on kaksi korttia, 
+//    joista toinen on arvoltaan 10-13 ja toinen ässä. Toteuta luokkaan PelaajanKasi 
+//    metodi joka tutkii onko kätenä blackjack.    
+    
+}
