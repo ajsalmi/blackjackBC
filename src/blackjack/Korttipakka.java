ce5e012bd22585963c7eb8653646aca116acf8a6
Mikko Kujala <m.kujala@live.com>
2019-08-29 09:47:45 +0100

    Korttipakkaan muutoksia

--- src/blackjack/Korttipakka.java
+++ src/blackjack/Korttipakka.java
@@ -1,15 +1,46 @@
+// Tehtävänanto tälle luokalle.
+//    Toteuta luokka Korttipakka, jolla on ArrayList<Kortti>-tyyppinen
+//    atribuutti pakka. Luokan oletuskonstruktori sijoittaa pakka-atribuuttiin
+//    halutun määrän 52 korttisia korttipakkoja numero- ja maajärjestyksessä.
+//    Toteuta luokkaan metodi sekoita, joka sekoittaa atribuutin pakka korttien
+//    järjestyksen. Toteuta lisäksi metodi jaaKortti, joka palauttaa pakan
+//    ensimmäisen kortin ja poistaa sen pakasta.
 package blackjack;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  *
  * @author Mikko Kujala
  */
 public class Korttipakka {
-//    Toteuta luokka Korttipakka, jolla on ArrayList<Kortti>-tyyppinen 
-//    atribuutti pakka. Luokan oletuskonstruktori sijoittaa pakka-atribuuttiin 
-//    halutun määrän 52 korttisia korttipakkoja numero- ja maajärjestyksessä. 
-//    Toteuta luokkaan metodi sekoita, joka sekoittaa atribuutin pakka korttien 
-//    järjestyksen. Toteuta lisäksi metodi jaaKortti, joka palauttaa pakan 
-//    ensimmäisen kortin ja poistaa sen pakasta.
-    
+
+    private ArrayList<Kortti> pakka = new ArrayList<>();
+
+    /**
+     *
+     * @param pakkoja on 52-kortin pakkojen määrä.
+     */
+    public Korttipakka(int pakkoja){
+        String[] maat = new String[]{"Hertta", "Ruutu", "Pata", "Risti"};
+        for (int i = 0; i < pakkoja; i++) {
+            for (String maa : maat) {
+                for (int j = 2; j <= 14; j++) {
+                    Kortti kortti = new Kortti(j, maa);
+                    this.pakka.add(kortti);
+                }
+            }
+        }
+    }
+
+    public void sekoita(){
+        Collections.shuffle(this.pakka);
+    }
+
+    public Kortti jaaKortti(){
+        Kortti ret = pakka.get(0);
+        pakka.remove(0);
+        return ret;
+    }
 }
