b851184322f05acd0dbe2fdaf35fda57e5f57406
Mikko Kujala <m.kujala@live.com>
2019-08-29 09:01:21 +0100

    Varasin korttipakan :D

--- src/blackjack/Korttipakka.java
+++ src/blackjack/Korttipakka.java
@@ -1,14 +1,8 @@
 package blackjack;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author 
+ * @author Mikko Kujala
  */
 public class Korttipakka {
 //    Toteuta luokka Korttipakka, jolla on ArrayList<Kortti>-tyyppinen 
