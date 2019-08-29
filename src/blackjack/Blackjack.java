76a8698ec715855b4654b97abdfcd9168053089e
Javanainen Jenni <s1901261@edu.bc.fi>
2019-08-29 11:53:58 +0300

    Jenni was here
    Testing testing

--- src/blackjack/Blackjack.java
+++ src/blackjack/Blackjack.java
@@ -1,38 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package blackjack;
 
-/**
- *
- * @author 
- */
 public class Blackjack {
     
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-//	Toteuta edellisten tehtävien luokkia hyödyntäen sovellusluokka Blackjack. 
-//      Sovelluksessa luodaan aluksi korttipakka ja sekoitetaan se. Tämän jälkeen 
-//      pelaajalle jaetaan kaksi korttia ja ilmoitetaan korttien summa. 
-//      Mikäli summa on alle 21, kysytään pelaajalta haluaako hän lisäkortin. 
-//      Haluttaessa lisäkortti jaetaan ja ilmoitetaan uusi summa. Tätä jatketaan 
-//      kunnes summa on suurempi tai yhtä suuri kuin 21 tai kunnes pelaaja ei 
-//      halua lisää kortteja. Lopuksi ilmoitetaan loppusumma.
-
-//	Lisää edelliseen sovellusluokkaan Blackjack korttien jakaminen 
-//      tietokoneavustajalle. Vastusta haluaa nostaa lisää kortteja niin kauan 
-//      kuin summa on alle 15. Lopuksi ilmoitetaan voittaja. Pelin voittaa se, 
-//      kummalla on suurempi summa, joka on korkeintaan 21. Summan 21 ylittänyt 
-//      pelaaja häviää aina.
-
         
         System.out.println("blackjack");
+        
+        System.out.println("Jenni was here!");
         //
         
     }
